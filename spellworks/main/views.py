7d983d4abff97b561dccce4385c3bdda1b4fe832
ethe_go <gotzehsing@gmail.com>
2015-11-09 23:06:52 +0800

    add sidebar

--- spellworks/main/views.py
+++ spellworks/main/views.py
@@ -2,13 +2,13 @@
 __author__ = 'zeno guo'
 
 from flask.views import MethodView
-from flask import jsonify
+from flask import jsonify, render_template
 
 
 class Index(MethodView):
 
     def get(self):
-        return "hello, world"
+        return render_template('test.html')
 
     def post(self, *args, **kw):
         return jsonify(status='ok')
