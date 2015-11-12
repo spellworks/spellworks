d5de91e0372b2b249995fd65051ec7cea8d2b245
ethe_go <gotzehsing@gmail.com>
2015-11-12 12:01:13 +0800

    complete font end post field

--- spellworks/login/views.py
+++ spellworks/login/views.py
@@ -2,9 +2,7 @@
 __author__ = 'zeno guo'
 
 from flask.views import MethodView
-from flask import jsonify
-# from . import models
-from main import views
+from flask import jsonify, request
 
 
 class LoginIndex(MethodView):
