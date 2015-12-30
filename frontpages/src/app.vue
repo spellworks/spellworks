4b99a041a701784c253d46152d079decabc716db
wb-guozixing <gotzehsing@gmail.com>
2015-12-30 19:27:04 +0800

    daily saved

---
+++ frontpages/src/app.vue
@@ -0,0 +1,21 @@
+<template lang="jade">
+  #app
+    img.logo(src="./assets/logo.png")
+    hello
+</template>
+
+<script>
+import hello from './components/hello'
+
+export default {
+  components: {
+    hello
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
--- frontpages/src/components/hello.vue
+++ frontpages/src/components/hello.vue
@@ -1,7 +1,6 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+<template lang="jade">
+  .hello
+    h1 {{ msg }}
 </template>
 
 <script>
